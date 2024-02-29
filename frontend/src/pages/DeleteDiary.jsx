18cffc2e99107210cb6c65a193428e453a0beef1
hydrotolerance <julzhafiz@gmail.com>
2024-02-29 19:06:19 +0800

    (Add) Update diary and also Delete diary

--- frontend/src/pages/DeleteDiary.jsx
+++ frontend/src/pages/DeleteDiary.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import Spinner from "../components/Spinner";
+import { BackButton } from "../components/BackButton";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function DeleteDiary() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const handleDeleteDiary = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:5555/diary/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("Error deleting diary!");
+        console.log(err);
+      });
+  };
   return (
-    <div>DeleteDiary</div>
-  )
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-center">Delete Diary</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-500 rounded-xl w-[600] p-8 mx-auto">
+        <h3 className="text-2xl">
+          Are you sure you want to delete this diary?
+        </h3>
+        <button
+          onClick={handleDeleteDiary}
+          className="bg-red-600 text-white m-8 w-full"
+        >
+          Yes delete it!
+        </button>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/UpdateDiary.jsx
+++ frontend/src/pages/UpdateDiary.jsx
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BackButton } from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function UpdateDiary() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [dateCreated, setdateCreated] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/diary/${id}`)
+      .then((res) => {
+        setTitle(res.data.title);
+        setAuthor(res.data.author);
+        setdateCreated(res.data.dateCreated);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("An error happened. Please check you input and try again");
+        console.log(err);
+      });
+  }, []);
+
+  const handleUpdateDiary = () => {
+    const data = {
+      title,
+      author,
+      dateCreated,
+    };
+    setLoading(true);
+    axios
+      .post("http://localhost:5555/diary", data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error happened. Please check your inputs and try again.");
+        console.log(error);
+      });
+  };
+
   return (
-    <div>UpdateDiary</div>
-  )
+    <div className="m-4p-4">
+      <BackButton />
+      <h1>Edit Diary</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-600 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-grey-500 px-4 py-2 w-full "
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-grey-500 px-4 py-2 w-full "
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Date Created</label>
+          <input
+            type="number"
+            value={dateCreated}
+            onChange={(e) => setdateCreated(e.target.value)}
+            className="border-2 border-grey-500 px-4 py-2 w-full "
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleUpdateDiary}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
